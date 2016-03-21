33cba6707a46c6f685a7731c305599bca2bcd4e0
mermoz21 <denis.malavieille@gmail.com>
2016-03-21 13:26:58 +0000

    Ceci est mon premier essais de commit

--- start-test.js
+++ start-test.js
@@ -1,17 +1,62 @@
+//==== Classe Connecteur ====
+function connector(){
+  this.push=push;
+  this.pull=pull;
+  this.call=call;
+  this.connect=connect;
+  this.callback=undefined;
+  this.value=undefined;
+  
+  function push(value){
+    
+    if (value!=undefined) {
+      this.value=value;
+    } 
+      
+    if ((this.callback!=undefined)&&(this.value!=undefined))  this.callback(this.value);
+  }
+  
+  function pull(){
+    var value=undefined;
+    
+    return value;
+  }
+  
+  function call(value){
+    return this.callback(value);
+  }
+  
+  function connect(callback){
+    this.callback=callback;
+    if (this.value!=undefined) {this.push()};
+  }
+  
+  function unconnect(){
+    this.callback=undefined;
+  }
+}
+//=======================
+
 //==== composant:app_expressjs====
-var express = require('express');
-var path = require('path');
-var parser=require("body-parser");
-
-var app = express();
-app.use(express.static(path.resolve(__dirname,'client')));
-app.post('/qrcode',parser.urlencoded(),myfn);
-
-function myfn(req,res){
-  console.log(req.body);
-  var image_svg=/*connecteur: image_svg*/ generate_qrcode(req.body.code,'svg');
-  res.type('svg');
-  image_svg.pipe(res);
+function app_expressjs(){
+  var express = require('express');
+  var path = require('path');
+  var parser=require("body-parser");
+  
+  this.app=new connector();
+  this.get_qrcode=new connector();
+  
+  var app = express();
+  app.use(express.static(path.resolve(__dirname,'client')));
+  app.post('/qrcode',parser.urlencoded(),myfn.bind(this));
+  this.app.push(app);
+  
+  function myfn(req,res){
+    //console.log(req.body);
+    var image_svg=this.get_qrcode.call({code:req.body.code,format:'svg'});
+    res.type('svg');
+    image_svg.pipe(res);
+  }
 }
 //=======================
 
@@ -19,7 +64,7 @@ function myfn(req,res){
 function Serveur_HTTP()
 {
   var http = require('http');
-  this.start=function start(app)
+  this.start=function (app)
   {
     var server = http.createServer(/*connecteur:request_listner*/app);
     
@@ -29,9 +74,15 @@ function Serveur_HTTP()
     });
   };
 }
-new Serveur_HTTP().start(app);
+
 //=======================
 
+var appjs=new app_expressjs();
+appjs.app.connect(new Serveur_HTTP().start);
+appjs.get_qrcode.connect(function (value){return generate_qrcode(value.code,value.format)});
+
+var t='string';
+console.log(t);
 //==== composant:generateur_qrcode ====
 var qr=require("qr-image");
 
